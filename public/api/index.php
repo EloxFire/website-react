e7c3bc53c1155837a5c04590470ddc8848943da3
EloxFire <enzo.avagliano@ynov.com>
2020-08-25 19:19:52 +0200

    Updated mailer php

--- public/api/index.php
+++ public/api/index.php
@@ -33,41 +33,4 @@ if (empty($_POST['username']) && empty($_POST['usermail'])){
   echo "Something went wrong";
   exit();
 }
-// if (empty($_POST['name']) && empty($_POST['mail'])) die();
-//
-// if ($_POST)
-// {
-//
-//   // set response code - 200 OK
-//
-//   http_response_code(200);
-//   $subject = $_POST['messageObject'];
-//   $to = "enzo.avagliano@ynov.com";
-//   $from = $_POST['mail'];
-//
-//   // data
-//
-//   $msg = $_POST['message'];
-//
-//   // Headers
-//
-//   $headers = "MIME-Version: 1.0\r\n";
-//   $headers.= "Content-type: text/html; charset=UTF-8\r\n";
-//   $headers.= "Vous avez reçu un message de la part de : " . $from . " !";
-//   mail($to, $subject, $msg, $headers);
-//
-//   // echo json_encode( $_POST );
-//
-//   echojson_encode(array(
-//     "sent" => true
-//   ));
-// }
-// else
-// {
-//
-//   // tell the user about error
-//
-//   echojson_encode(["sent" => false, "message" => "Something went wrong"]);
-// }
-
 ?>
