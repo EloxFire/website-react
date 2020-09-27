bc9f6738e2733ce163668735619ab21911b0e5f1
EloxFire <enzo.avagliano@ynov.com>
2020-09-27 22:53:40 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import About from './components/About';
+
 class App extends Component{
   componentDidMount(){
     document.title = "Enzo Avagliano - Home";
@@ -10,16 +13,25 @@ class App extends Component{
     return (
       <div id="website">
         <Router>
+
+          <Navbar titles={["About", "My work", "Blog", "Devdeas", "Blinxon"]}/>
           <Switch>
             <Route path="/" exact component={props =>
               <div>
+                <About/>
               </div>
             }/>
 
             <Route path="/home" component={props =>
               <div>
+                <About/>
               </div>
             }/>
+
+            <Route path='/about' component={() => {
+              window.location.href = '/home';
+              return null;
+            }}/>
           </Switch>
         </Router>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import logo from '../img/logos/ea.svg';
+import '../sass/navbar.scss';
 
 interface NavbarProps{
   titles: String[],
 }
-const Navbar:React.FC<NavbarProps> = ({titles}) => {
-  <div>
-    test
-  </div>
-}
+const Navbar:React.FC<NavbarProps> = ({titles}) => (
+  <nav id="navbar" className="navbar navbar-expand-lg sticky-top">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <a className="navbar-brand" href="/home">
+      <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="Enzo Avagliano's logo"/>
+    </a>
+
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+        {titles.map(function(link, key){
+          return(
+            <li key={key} className="nav-item mr-2 ml-2">
+              <a className="nav-link" href={link.toLowerCase().replace(/\s/g, '')}>{link}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  </nav>
+)
 
 export default Navbar;
