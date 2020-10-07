88e79a4a0ded6c9076f785a1b32b0a1514ed818a
EloxFire <enzo.avagliano@ynov.com>
2020-10-07 18:21:13 +0200

    About page finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import Progressbar from './sub/Progressbar';
 
 import logo from '../img/logos/ea.svg';
 import pp from '../img/cvpp.png';
+import resume from '../img/CV_AVAGLIANO.pdf';
 import '../sass/pages.scss';
 
 class About extends Component{
@@ -36,20 +37,26 @@ class About extends Component{
                   <hr/>
                 </div>
                 <p style={{fontWeight:"bold"}}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam iure, neque maiores repellat excepturi cupiditate vitae a aperiam iste. Facilis vero alias laborum ipsum cumque delectus sapiente dignissimos voluptatum praesentium.
+                  I'm a 20 years old french computer engineering student in his 3rd year at Ynov Campus, in Aix-en-Provence. I'm very curious and want to learn very various things.
+                  <br/>I'm involved in many projects and try to develop my own. Fun, joy of life and efficiency are priorities in my work.
                 </p>
+
+                <a download href={resume}><button className="btn btn-primary btn-lg bg-purple m-1"><Icon.CloudDownloadFill color="white"/> Download my resume.</button></a>
+                <a href="https://enzoavagliano.fr/contact"><button className="btn btn-primary btn-lg bg-purple m-1"><Icon.TelephoneFill color="white"/> Contact me.</button></a>
               </div>
             </div>
           </div>
           <div className="col-12 col-lg-6 p-3 d-flex flex-column align-items-center align-items-lg-start justify-content-center">
-            <Title title="Mes compétences"/>
-            <div className="h-100 w-100 d-flex flex-column">
-              <div className="d-flex flex-column">
-                <Progressbar title="ReactJS" logo="React" value="60"/>
-                <Progressbar title="NodeJS" logo="Node" value="50"/>
-                <Progressbar title="HTML" logo="Html" value="50"/>
-                <Progressbar title="SASS" logo="Sass" value="50"/>
-                <Progressbar title="Git" logo="Git" value="50"/>
+            <Title title="My main skills"/>
+            <div className="h-75 w-100 d-flex flex-column">
+              <div className="d-flex flex-column h-100 justify-content-around">
+                {/*Niveaux : 0 - 30 Débutant 35 - 65 Intermétiaire 65 - 100 - Avancé*/}
+                <Progressbar title="Levels legend" value="100" level="0-35% = Beginner | 35-65% = Intermediate | 65-100% = Advanced"/>
+                <Progressbar title="ReactJS" logo="react.svg" value="65" level="Intermediate"/>
+                <Progressbar title="NodeJS" logo="node.svg" value="45" level="Beginner"/>
+                <Progressbar title="HTML" logo="html.svg" value="90" level="Advanced"/>
+                <Progressbar title="SASS/CSS" logo="sass.svg" value="90" level="Advanced"/>
+                <Progressbar title="Git" logo="github.svg" value="75" level="Advanced"/>
               </div>
             </div>
           </div>
