08d608677a60d9978001061c1b14cfb5aa9ae7d0
EloxFire <enzo.avagliano@ynov.com>
2020-10-11 00:54:07 +0200

    Added certif idea form front + website text panel

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,6 +3,25 @@ import * as Icon from 'react-bootstrap-icons';
 import '../sass/pages.scss';
 
 class Dashboard extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      ideas: [],
+    }
+  }
+
+  getDevdeasIdeas(){
+    fetch("https://projects-enzoavagliano.fr:9000/getideas")
+    .then(res => res.json())
+    .then(res => this.setState({
+      ideas: res
+    }));
+  }
+
+  componentDidMount(){
+    this.getDevdeasIdeas();
+  }
+
   render(){
     return(
       <div id="dashboard" className="d-flex flex-column flex-lg-row">
@@ -18,6 +37,9 @@ class Dashboard extends Component{
             <a className="nav-link" id="v-pills-certifyideas-tab" data-toggle="pill" href="#v-pills-certifyideas" role="tab" aria-controls="v-pills-certifyideas" aria-selected="true">Certify ideas</a>
             <a className="nav-link" id="v-pills-removeideas-tab" data-toggle="pill" href="#v-pills-removeideas" role="tab" aria-controls="v-pills-removeideas" aria-selected="false">Remove ideas</a>
             <a className="nav-link" id="v-pills-updateideas-tab" data-toggle="pill" href="#v-pills-updateideas" role="tab" aria-controls="v-pills-updateideas" aria-selected="false">Update idea fields</a>
+
+            <h4 className="mt-5">Website</h4>
+            <a className="nav-link" id="v-pills-updatetext-tab" data-toggle="pill" href="#v-pills-updatetext" role="tab" aria-controls="v-pills-updatetext" aria-selected="true">Update text</a>
           </div>
         </div>
         <div className="tab-content p-4 col-12 col-lg-9" id="v-pills-tabContent">
@@ -33,12 +55,41 @@ class Dashboard extends Component{
 
           <div className="tab-pane fade" id="v-pills-certifyideas" role="tabpanel" aria-labelledby="v-pills-certifyideas-tab">
             <h2>Certify an idea :</h2>
+            <div className="mt-5">
+              <form class="form-inline">
+                <div class="form-group mx-sm-3 mb-2">
+                  <label className="white" htmlFor="selectIdeaToCertify">Idea to certify :</label>
+                  <select id="selectIdeaToCertify" class="form-control form-control-sm">
+                    <option>An idea</option>
+                    <option>An other idea</option>
+                    <option>Idea 3</option>
+                  </select>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2 bg-purple">Certify idea</button>
+              </form>
+            </div>
+            <div className="ideasPanel col-12 col-lg-12 pt-3">
+              <h4 className="white">Idea list :</h4>
+              {
+                this.state.ideas.map(function(item, index){
+                  return(
+                    <li className="white mt-3" style={{listStyleType: "none",textTransform:"uppercase",fontWeight:"bold"}} key={index}>
+                      {item.name} <small className="white">{item.description}</small>
+                    </li>
+                  )
+                })
+              }
+            </div>
           </div>
           <div className="tab-pane fade" id="v-pills-removeideas" role="tabpanel" aria-labelledby="v-pills-removeideas-tab">
             <h2>Remove an idea :</h2>
           </div>
           <div className="tab-pane fade" id="v-pills-updateideas" role="tabpanel" aria-labelledby="v-pills-updateideas-tab">
             <h2>Update an idea :</h2>
+          </div>
+
+          <div className="tab-pane fade" id="v-pills-updatetext" role="tabpanel" aria-labelledby="v-pills-updatetext-tab">
+            <h2>Update a text on the website :</h2>
           </div>
         </div>
       </div>
