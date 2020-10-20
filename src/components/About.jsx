eef37fec20573f9abe1b2b1ea30775af4987c93a
EloxFire <enzo.avagliano@ynov.com>
2020-10-20 14:50:47 +0200

    Optimized css by actualy using SASS powers

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ class About extends Component{
   render(){
     return(
       <div id="about" className="">
-        <div className="landing">
+        <div id="landing" className="landing">
           <div className="h-100 col-12 d-flex flex-column flex-lg-row align-items-center justify-content-center">
             <div className="mb-3 mb-lg-0 mr-lg-3"><img className="landing-logo" src={logo} alt="Enzo Avagliano' logo"/></div>
             <div className="d-flex flex-column align-items-center align-items-lg-start">
