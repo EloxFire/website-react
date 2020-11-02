dd7dd62b817ceffbe9b90f1904240ee216061630
EloxFire <enzo.avagliano@ynov.com>
2020-11-02 12:12:05 +0100

    V1 Work page done + .gitattribute

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -59,23 +59,74 @@ class Work extends Component {
                 <img style={{maxWidth: "100%"}} src={this.state.profile.avatar_url} alt="Enzo Avagliano's logo"/>
               </div>
               <div className="col-12 col-lg-9 white mt-5 mt-lg-0">
-                <h3><FIcons.FaUser/> {this.state.profile.name}</h3>
+                <h3 className="mb-4 border-bottom pb-2"><FIcons.FaUser/> {this.state.profile.name}</h3>
                 <h5><FIcons.FaGithub/> {this.state.profile.login}</h5>
+                <h5><FIcons.FaTwitter/> Twitter</h5>
                 <h5><FIcons.FaBirthdayCake/> Nov 9, 2000</h5>
-                <h5><FIcons.FaCode/> {this.state.profile.bio}</h5>
-                <h5>Public repositories : {this.state.repos.length}</h5>
-                <h5>Twitter</h5>
+                <h5><FIcons.FaBook/> Public repositories : {this.state.repos.length}</h5>
+                <div className="d-flex flex-column">
+                  <h5><FIcons.FaCode/> BIO :</h5>
+                  <p>{this.state.profile.bio}</p>
+                </div>
               </div>
             </div>
           </div>
           <div className="tab-pane fade pl-0 pl-lg-5" id="v-pills-repos" role="tabpanel" aria-labelledby="v-pills-repos-tab">
             <h1 className="white">My repositories.</h1>
+            <p className="white">Organized in alphabetical order [A-Z]</p>
+            <div className="p-4 col-12 d-flex flex-wrap white justify-content-center">
+            {
+              this.state.repos.map(function(item, index){
+                return(
+                  <div key={index} className="repo-card m-3 p-2">
+                    <div className="repo-header">
+                      <h4 className="text-truncate">{item.name}</h4>
+                    </div>
+                    <div className="repo-body p-2">
+                      {
+                        item.description != null &&
+                        <p>{item.description}</p>
+                      }
+
+                      {
+                        item.description == null &&
+                        <p className="mr-2"><FIcons.FaBan/> No description</p>
+                      }
+                    </div>
+                    <div className="repo-lang d-flex align-items-baseline pl-2">
+                      {
+                        item.language != null &&
+                        <p className="mr-2"><FIcons.FaFileCode/> {item.language}</p>
+                      }
+
+                      {
+                        item.language == null &&
+                        <p className="mr-2"><FIcons.FaFileCode/> Not specified</p>
+                      }
+                    </div>
+                    <div className="repo-update d-flex align-items-baseline pl-2">
+                      <p><FIcons.FaRedo/> : <Timestamp relative date={item.updated_at}/></p>
+                    </div>
+                    <div className="repo-footer">
+                      <a className="nav-link white" target="_blank" href={item.html_url}>View on <FIcons.FaGithub/></a>
+                    </div>
+                  </div>
+                )
+              })
+            }
+            </div>
           </div>
           <div className="tab-pane fade pl-0 pl-lg-5" id="v-pills-school-projects" role="tabpanel" aria-labelledby="v-pills-school-projects-tab">
             <h1 className="white">My school projects.</h1>
+            <div className="p-4 col-12 d-flex flex-wrap white justify-content-center">
+              <h4>Specific sections are coming soon.</h4>
+            </div>
           </div>
           <div className="tab-pane fade pl-0 pl-lg-5" id="v-pills-personnal-initiative" role="tabpanel" aria-labelledby="v-pills-personnal-initiative-tab">
             <h1 className="white">My personnal initiatives.</h1>
+            <div className="p-4 col-12 d-flex flex-wrap white justify-content-center">
+              <h4>Specific sections are coming soon.</h4>
+            </div>
           </div>
         </div>
       </div>
